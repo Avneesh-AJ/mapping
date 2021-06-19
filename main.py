de1c9b591cd9ba09fd10b36688e4f774f79d3d2b
Avneesh Jha <avneeshjha2015@gmail.com>
2021-06-20 02:13:53 +0530

    adding a html popup

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ lat=list(data["LAT"])
 lon=list(data["LON"])
 elev=list(data["ELEV"])
 
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
 map=folium.Map(location=[38,-99.09],zoom_start=6, tiles="Stamen Terrain")
 fg=folium.FeatureGroup(name ="My Map")
 for lt,ln,ele in (zip(lat,lon,elev)):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(ele)+"mtrs",icon=folium.Icon(color='green')))
+    iframe=folium.IFrame(html= html % str(ele), width =200 ,height = 100)
+    fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe),icon=folium.Icon(color='green')))
 map.add_child(fg)
 map.save("localhost.html")

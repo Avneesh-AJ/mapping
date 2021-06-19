15abb40891d8f6ba5a608e4e8928e3ef1bce4699
Avneesh Jha <avneeshjha2015@gmail.com>
2021-06-20 01:44:34 +0530

    First draft of working code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import folium
 import pandas
+
+data=pandas.read_csv("Volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+
+map=folium.Map(location=[38,-99.09],zoom_start=6, tiles="Stamen Terrain")
+fg=folium.FeatureGroup(name ="My Map")
+for lt,ln,ele in (zip(lat,lon,elev)):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="IT'S A MARK",icon=folium.Icon(color='green')))
+map.add_child(fg)
+map.save("localhost.html")

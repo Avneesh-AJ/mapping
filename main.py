363f4808dc0b086787570079d98130413efee9fd
Avneesh Jha <avneeshjha2015@gmail.com>
2021-07-04 16:58:07 +0530

    Color coding added to different elevations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import folium
+from jinja2.environment import get_spontaneous_environment
 import pandas
 
 data=pandas.read_csv("Volcanoes.txt")
@@ -6,6 +7,16 @@ lat=list(data["LAT"])
 lon=list(data["LON"])
 elev=list(data["ELEV"])
 name = list(data["NAME"])
+
+def color_gen(elevation):
+    if 0 <= elevation < 1000:
+        return "green"  
+    elif  1000<=elevation<2000:
+        return "purple"
+    elif 2000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
 
 
 html = """
@@ -17,6 +28,6 @@ map=folium.Map(location=[38,-99.09],zoom_start=6, tiles="Stamen Terrain")
 fg=folium.FeatureGroup(name ="My Map")
 for lt,ln,ele,name in (zip(lat,lon,elev,name)):
     iframe=folium.IFrame(html= html % (name, name, ele), width =200 ,height = 100)
-    fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe),icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe),icon=folium.Icon(color=color_gen(ele))))
 map.add_child(fg)
 map.save("localhost.html")
